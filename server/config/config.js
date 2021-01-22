c996fef12e26aca1384253de2a874edaceea5044
Julius Guevarra <juliusguevarra101@gmail.com>
2021-01-22 16:32:26 +0800

    🛠 Troubleshooting heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const path = require('path');
-const env = process.env.NODE_ENV || 'dev';
+const env = process.env.NODE_ENV || 'prod';
 
-if (env !== 'prod') {
+if (env === 'dev') {
   dotenv.config({ path: path.join(__dirname, '../.env-dev') });
 }
