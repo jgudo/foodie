0e69e4fe0a73e2939c09ec7072622d3883e90a1b
Julius Guevarra <juliusguevarra101@gmail.com>
2021-01-22 16:23:17 +0800

    🛠 Removed dotend config for production

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 const env = process.env.NODE_ENV || 'dev';
 
-if (env === 'dev') {
+if (env !== 'prod') {
   dotenv.config({ path: path.join(__dirname, '../.env-dev') });
-} else {
-  dotenv.config({ path: path.join(__dirname, '../.env-prod') });
 }
