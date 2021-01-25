9a934a0af6f870967eabd2ddbeb9a7560703926d
Julius Guevarra <juliusguevarra101@gmail.com>
2021-01-25 09:46:08 +0800

    🛠 Added socket connection options

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,5 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_FOODIE_URL || 'http://localhost:9000');
+const socket = io(process.env.REACT_APP_FOODIE_URL || 'http://localhost:9000', {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    reconnectionAttempts: 50
+});
 
 export default socket;
