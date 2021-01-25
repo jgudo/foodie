189e91e46516482cb37871b1659bff061605a347
Julius Guevarra <juliusguevarra101@gmail.com>
2021-01-25 09:56:15 +0800

    🛠 Adjust newsfeed error container height

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -117,7 +117,7 @@ const Home: React.FC<RouteComponentProps<any, any, ILocation>> = (props) => {
                     dispatchCreatePost={dispatchCreatePost}
                 />
                 {(state.error && state.newsFeed.items.length === 0) && (
-                    <div className="flex flex-col w-full min-h-screen items-center justify-center">
+                    <div className="flex flex-col w-full min-h-24rem items-center justify-center">
                         <CoffeeOutlined className="text-8xl text-gray-300 mb-4" />
                         <h5 className="text-gray-500">News feed is empty</h5>
                         <p className="text-gray-400">Start following people or create your first post.</p>
