3e175be8c3277edd84f09df5efbd1f6fe755f07e
Julius Guevarra <juliusguevarra101@gmail.com>
2021-02-07 14:32:45 +0800

    🛠 Fix CORS issues.

--- server/src/app.ts
+++ server/src/app.ts
@@ -33,13 +33,21 @@ class Express {
 
   private initializeMiddlewares() {
     this.app.disable('x-powered-by');
-    this.app.set('trust proxy', 1);
     this.app.use(cors(config.cors));
+    this.app.set('trust proxy', 1);
     this.app.use(logger('dev'));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(helmet());
     this.app.use(hpp());
+
+    this.app.use(
+      helmet({
+        referrerPolicy: {
+          policy: 'origin-when-cross-origin',
+        },
+      })
+    )
 
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: false }))
