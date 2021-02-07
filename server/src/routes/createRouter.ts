35d3c75cb3d69293a2de67b3a59dd053e0b73e5d
Julius Guevarra <juliusguevarra101@gmail.com>
2021-02-07 16:47:40 +0800

    🛠 Troubleshoot heroku.

--- server/src/routes/createRouter.ts
+++ server/src/routes/createRouter.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import glob from 'glob';
 
 const routers = glob
-    .sync('**/*.ts', { cwd: `${__dirname}/` })
+    .sync(`**/*.${process.env.NODE_ENV === 'dev' ? 'ts' : 'js'}`, { cwd: `${__dirname}/` })
     .map((filename: string) => require(`./${filename}`))
     .filter((router: any) => {
         return router.default && Object.getPrototypeOf(router.default) === Router
